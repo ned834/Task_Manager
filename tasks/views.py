6504593dc651603a19dc8ef54a94d429e7e5b59c
Ninaad Naddanthadi <ninaadnaddanthadi@Ninaads-MacBook-Air.local>
2026-04-20 18:33:35 +0530

    Created task model, admin, and implemented basic features

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Task
 
-# Create your views here.
+def home(request):
+    if request.method == "POST":
+        title = request.POST.get("title")
+        due_date = request.POST.get("due_date")
+        if title:
+            Task.objects.create(title=title, due_date=due_date if due_date else None)
+        return redirect("/")
+
+    tasks = Task.objects.all()
+    return render(request, "tasks/home.html", {"tasks": tasks})
+
+    
+
+def complete_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+    task.completed = not task.completed
+    task.save()
+    return redirect("/")
+
+def delete_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+    task.delete()
+    return redirect("/")
