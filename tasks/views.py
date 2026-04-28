75b3cda27239ef09df1307af13d5f55c1d007579
Ninaad Naddanthadi <nninaad07@gmail.com>
2026-04-28 22:53:43 +0530

    Implemented priority sorting and filtering

--- tasks/views.py
+++ tasks/views.py
@@ -2,22 +2,50 @@ from django.shortcuts import render, redirect
 from .models import Task
 from datetime import date
 
+
+today = date.today()
+
 def home(request):
     if request.method == "POST":
         title = request.POST.get("title")
         due_date = request.POST.get("due_date")
+
         if title:
-            Task.objects.create(title=title, due_date=due_date if due_date else None)
+            Task.objects.create(
+                title=title,
+                due_date=due_date if due_date else None
+            )
         return redirect("/")
 
-    tasks = Task.objects.order_by('completed', 'due_date')
 
-    today = date.today()
+    tasks = Task.objects.all()
+    filter_type = request.GET.get("filter")
+
+    
+    if filter_type == "active":
+        tasks = tasks.filter(completed=False)
+    elif filter_type == "completed":
+        tasks = tasks.filter(completed=True)
+
+    
+    tasks = sorted(
+        tasks,
+        key=lambda task: (
+            task.completed,
+            0 if task.due_date and task.due_date < today else
+            1 if task.due_date == today else
+            2 if task.due_date else
+            3,
+            task.due_date or today
+        )
+    )
 
     return render(request, "tasks/home.html", {
         "tasks": tasks,
-        "today": today
+        "today": today,
+        "current_filter": filter_type
     })
+
 
 def complete_task(request, task_id):
     task = Task.objects.get(id=task_id)
