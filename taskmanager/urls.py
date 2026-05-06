118a3274533e2fb233da41d654098b0d73d4af87
Ninaad Naddanthadi <nninaad07@gmail.com>
2026-05-06 21:03:00 +0530

    Added cards for tasks

--- taskmanager/urls.py
+++ taskmanager/urls.py
@@ -21,8 +21,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('tasks.urls')),
-
-    
     path('login/', auth_views.LoginView.as_view(template_name='tasks/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/login/'), name='logout'),
 ]
